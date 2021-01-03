11a540be64b7f32a5830e39e907249ec286773ab
nhd2106 <nhd.ars@gmail.com>
2021-01-03 13:04:45 +0700

    fixed styled

--- pages/dia-diem.js
+++ pages/dia-diem.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { Container } from "@material-ui/core";
-import { LienHeStyles } from "./styles";
 
 export default function LienHe(props) {
-  return <LienHeStyles>
+  return <>
 
       
-  </LienHeStyles>;
+  </>;
 }
