972e8ec8a1f6b404ea54fe0c1b52c3b55e21dc7c
Duoc Ngo <nhd.ars@gmail.com>
2021-02-26 12:50:26 +0700

    config

--- libs/config.js
+++ libs/config.js
@@ -1,6 +1,6 @@
 export const BACKEND = () => {
-    if (process.env.NODE_ENV === 'development') return 'http://yeuvivu.vn:1337';
+    if (process.env.NODE_ENV === 'development') return 'https://157.230.46.108';
     // if (window.location.href.indexOf('stedu.vn') !== -1) return 'https://backend.stedu.vn:1237';
-    return 'http://yeuvivu.vn:1337';
+    return 'https://157.230.46.108';
   };
 
