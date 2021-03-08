19d7708b4f515dd7b691bc7f5c48a6e29c3b24bf
duoc ngo <nhd.ars@gmail.com>
2021-03-08 22:52:09 +0700

    signin

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,9 +1,17 @@
 import { Editor } from '@tinymce/tinymce-react';
 
 const Admin = (props) => {
-    const handleEditorChange = (content, editor) => {
-        console.log('Content was updated:', content);
+    
+  const handleChange = data => {
+    if (data.mime.includes('image')) {
+      const imgTag = `<p><img src="${data.url}" caption="${data.caption}" alt="${data.alternativeText}"></img></p>`;
+      const newValue = value ? `${value}${imgTag}` : imgTag;
+
+      onChange({ target: { name, value: newValue } });
     }
+
+    // Handle videos and other type of files by adding some code
+  };
 
     return (
         <Editor
@@ -11,17 +19,31 @@ const Admin = (props) => {
           init={{
             height: 500,
             menubar: false,
+            // eslint-disable-next-line @typescript-eslint/camelcase
+            convert_urls: false,
+            relative_urls : true,
+            remove_script_host : true,
+            toolbar_mode: 'wrap',
             plugins: [
               'advlist autolink lists link image charmap print preview anchor',
               'searchreplace visualblocks code fullscreen',
-              'insertdatetime media table paste code help wordcount'
+              'insertdatetime media table paste code help wordcount',
+              'media codesample fullscreen',
+              'hr visualchars imagetools emoticons'
             ],
             toolbar:
-              'undo redo | formatselect | bold italic backcolor | \
+            // eslint-disable-next-line no-multi-str
+              'undo redo | formatselect forecolor backcolor | \
+              bold italic underline strikethrough removeformat | \
               alignleft aligncenter alignright alignjustify | \
-              bullist numlist outdent indent | removeformat | help'
+              outdent indent | numlist bullist | \
+              table link anchor | image media codesample charmap emoticons | \
+              fullscreen code'
           }}
-          onEditorChange={handleEditorChange}
+          onEditorChange={(content, editor) => {
+            handleChange({ target: { name, value: content } });
+          }}
+          apiKey="t8ck8t9qgh3ezs4rwpivbdaduzgqta0hp1ca61xlo5z7yuyv"
         />
       );
 }
